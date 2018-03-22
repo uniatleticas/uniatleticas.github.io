a114bcf5c3e7e95a0402a12e6bea70bf5cbda98e
uniatleticas <37673888+uniatleticas@users.noreply.github.com>
2018-03-22 17:51:29 -0300

    Upload inicial do Site.
    Esqueleto básico de Login e Cadastro.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function confere_cadastro(){
+	var senha = document.getElementById("senha").value;
+	var confirmasenha = document.getElementById("confirmasenha").value;
+
+	if(senha != confirmasenha){
+		window.alert("Senhas não Conferem. Por favor,tente novamente.");
+	}
+}
