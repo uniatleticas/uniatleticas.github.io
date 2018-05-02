31decd2f41bc16c640007e4916ec417d7579e525
Gian Michel <31005220+gianveloxbr@users.noreply.github.com>
2018-05-01 21:42:32 -0300

    Atualizado arquivo sql
    Criar database uniatleticas e importar o arquivo usuarios.sql dentro dela.

--- usuarios.sql
+++ usuarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 02-Maio-2018 às 00:37
+-- Generation Time: 02-Maio-2018 às 00:43
 -- Versão do servidor: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -21,6 +21,42 @@ SET time_zone = "+00:00";
 --
 -- Database: `uniatleticas`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `msg`
+--
+
+DROP TABLE IF EXISTS `msg`;
+CREATE TABLE IF NOT EXISTS `msg` (
+  `id_msg` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL,
+  `msg` varchar(140) NOT NULL,
+  `data_inclusao` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_msg`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `msg`
+--
+
+INSERT INTO `msg` (`id_msg`, `id_usuario`, `msg`, `data_inclusao`) VALUES
+(4, 1, 'Teste de projeto integrador UniAtleticas', '2018-04-17 21:54:45'),
+(5, 1, 'Bem vindo ao twitter', '2018-04-17 21:59:29'),
+(6, 1, 'eae men', '2018-04-17 22:01:44'),
+(7, 1, 'opa', '2018-04-18 10:11:52'),
+(8, 1, 'jc', '2018-04-18 10:20:27'),
+(9, 1, 'Mari', '2018-04-18 10:42:16'),
+(10, 1, 'Marii', '2018-04-18 17:24:31'),
+(11, 2, 'opa tudo bom ', '2018-04-18 20:31:10'),
+(12, 3, 'eae gnt', '2018-04-18 20:32:01'),
+(13, 3, 'asfsdfsg', '2018-04-18 20:52:44'),
+(14, 4, 'eae pessoa', '2018-04-18 21:07:07'),
+(15, 1, 'oi', '2018-04-18 21:08:41'),
+(16, 1, 'eae', '2018-04-19 18:39:07'),
+(17, 1, 'oooopa', '2018-04-19 18:49:22'),
+(18, 1, 'eae men', '2018-04-19 18:55:40');
 
 -- --------------------------------------------------------
 
@@ -57,6 +93,35 @@ INSERT INTO `usuarios` (`id`, `usuario`, `nome`, `sobrenome`, `email`, `confirma
 (7, 'teste', 'teste', 'fulano', 'teste@teste.com', 'teste@teste.com', 'f9f240a02ee017ad04834e6516b8f2ce', 'f9f240a02ee017ad04834e6516b8f2ce', 'SI', 'Patos de Minas'),
 (8, 'teste2', 'Jonas', 'Carvalho', 'jonas@teste.com', 'jonas@teste.com', '81dc9bdb52d04dc20036dbd8313ed055', '81dc9bdb52d04dc20036dbd8313ed055', 'SI', 'Carmo do Paranaíba'),
 (9, 'gianvelox', 'Gian', 'Michel', 'gianveloxsi@gmail.com', 'gianveloxsi@gmail.com', 'a8e0a09388d92d2a17633e4e5adbdc66', 'a8e0a09388d92d2a17633e4e5adbdc66', 'SI', 'Patos de Minas');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios_seguidores`
+--
+
+DROP TABLE IF EXISTS `usuarios_seguidores`;
+CREATE TABLE IF NOT EXISTS `usuarios_seguidores` (
+  `id_usuario_seguidor` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL,
+  `seguindo_id_usuario` int(11) NOT NULL,
+  `data_registro` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_usuario_seguidor`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuarios_seguidores`
+--
+
+INSERT INTO `usuarios_seguidores` (`id_usuario_seguidor`, `id_usuario`, `seguindo_id_usuario`, `data_registro`) VALUES
+(12, 2, 1, '2018-04-18 20:16:29'),
+(13, 2, 3, '2018-04-18 20:31:18'),
+(14, 3, 1, '2018-04-18 20:31:45'),
+(15, 3, 2, '2018-04-18 20:31:50'),
+(16, 4, 1, '2018-04-18 21:06:42'),
+(17, 4, 2, '2018-04-18 21:06:48'),
+(18, 4, 3, '2018-04-18 21:06:53'),
+(20, 1, 4, '2018-04-18 21:54:56');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
